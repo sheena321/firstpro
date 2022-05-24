951b9b092ad8c578c7bce4ddb9c9d4d2dacb53e4
sheena <sheena3891@gmail.com>
2022-05-24 11:51:46 +0530

    my first commit

---
+++ resources/views/second.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+li{
+   display:inline;
+   background-color: chocolate;
+   padding: 20px;
+}
+a{
+    text-decoration: none;
+}
+/* .display
+{
+    display:block;
+} */
+p a{
+    display:block;
+}
+h2{
+    display: none;
+}
+h3{
+    visibility: hidden;
+}
+</style>
+</head>
+<body>
+    <ul>
+        <li><a href=" css">css</a> </li>
+        <li><a href="php">pghp</a> </li>
+        <li><a href=" java">java</a></li>
+    </ul>
+    <p>
+
+    <a href="" class="display">car</a>
+    <a href=""  class="display">bus</a>
+    <a href=""  class="display">cycle</a></p>
+    <h2>hai hello</h2>
+    <h3>new tag</h3>
+    <p>Lorem ipsum dolor  sit amet consectetur, adipisicing elit. At, neque!</p>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/first.css">
+    <style>
+        /* h1{
+            color: blue;
+            background-color:blueviolet;
+        }
+         */
+        /* .n1{
+             color: blue;
+             background-color:red;
+         } */
+        #n2 {
+            color: aqua;
+            background-color: blueviolet;
+        }
+
+        #n3 {
+            color: beige;
+            background-color: blue;
+        }
+
+        p,
+        h1 {
+            color: rgb(244, 244, 5);
+
+        }
+
+        div+p {
+            color: rgb(10, 49, 243);
+        }
+
+        p span {
+            color: rgb(100, 0, 75)
+        }
+        #n5
+        {
+            font-family: 'Courier New', Courier, monospace;
+            padding: auto;
+        }
+        h4{
+        /* padding: top 5px;
+        padding:left 4px ; */
+        /* padding: 20px */
+        /* padding: 10px; */
+        /* border: 1px solid blue; */
+        /* border- width:2px; */
+        border-width :10px 50px;
+        /* border-style:dotted; */
+        border-color:blue;
+        border-style:solid dotted dotted solid;
+        /* border-top-style:groove; */
+        border-radius: 20px;
+        margin: 20px 50px 40px 30px;
+        /* margin-top:30px; */
+        /* margin: auto; */
+
+        }
+        
+    </style>
+</head>
+
+<body>
+    <h1 style="color: blue; background-color: red;">gggg</h1>
+    <h1 class="n1">class selector1</h1>
+    <h1 class="n1">class selector2</h1>
+    <h1 id="n2">id selector1</h1>
+    <h3 id="n3">id selector2</h3>
+    <p class="n1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi iusto voluptates ullam. Dolorem
+        dolore sapiente magni accusantium commodi libero perspiciatis!</p>
+    <h2>global selector</h2>
+    <p class="n1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, eius!</p>
+    <div >
+        <H3> div started welcome</H3>
+        <h3>second div element</h3>
+    
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            At dolorum voluptates explicabo quas libero dolor voluptatem molestias soluta id! Asperiores consequatur in
+            incidunt magnam, est ipsam corporis tempore doloremque alias labore, veniam necessitatibus id cupiditate
+            facilis culpa obcaecati neque quo dolorum laborum. Impedit iusto esse facere deserunt est repellat minima.
+        </p>
+    </div>
+
+    <p >Lorem ipsum dolor sit <span> jjjjjj</span> consectetur adipisicing elit. Debitis, non.</p>
+    <input type="text">
+    <p id="n5" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, cupiditatevvvvvvvvvvvvvvvv.</p><br><br><br><br><br><br><br>
+
+<h4>padding</h4>
+<span>
+    gfhgk
+</span>
+
+</body>
+
+</html>
